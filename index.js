3fb115de6962c40f831c1f64ad75e0ce7f7a4b65
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-07-19 18:39:24 +0530

    ISO date getter

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let printHelloWorld = function() {
   console.log("Hello World...!");
-}
+};
+
+let getISODate = function() {
+  dateobj = new Date('July 15, 2018 05:35:32');
+  console.log(dateobj.toISOString());
+};
 
 printHelloWorld();
+getISODate();
